683f2252411799d3e8f73978fd7b098aec17734a
Arnaud Boulanger <arnaudboulanger@macbook-pro-2.home>
2020-08-11 12:07:30 +0200

    modif à analyser

--- programme.py
+++ programme.py
@@ -2,13 +2,16 @@ from __future__ import print_function
 from mysql.connector import (connection)
 from mysql.connector import errorcode
 import mysql.connector
+import json
+import urllib.request
 
+url = urllib.request.urlopen('https://fr.openfoodfacts.org/categorie/pates-a-tartiner/1.json')
+json_data= url.read()
+json_obj = [json.loads(json_data)]
 
 cnx = connection.MySQLConnection(user='root', password='Arnaud31',
                                  host='127.0.0.1',
                                  database='purbeurre')
-
-print(cnx)
 
 DB_NAME = 'purbeurre'
 
@@ -24,14 +27,14 @@ TABLES['storage'] = (
 TABLES['food_datas'] = (
     "CREATE TABLE IF NOT EXISTS `food_datas` ("
     "`id` INT NOT NULL,"
-    "`name` VARCHAR(45) NOT NULL,"
-    "`brand` VARCHAR(45) NOT NULL,"
-    "`nutriscore` CHAR(1) NOT NULL,"
-    "`store` VARCHAR(45) NOT NULL,"
-    "`URL` VARCHAR(45) NOT NULL,"
+    "`generic_name_fr` VARCHAR(45) NOT NULL,"
+    "`brands_tags` VARCHAR(45) NOT NULL,"
+    "`nutrition_grade_fr` CHAR(1) NOT NULL,"
+    "`stores` VARCHAR(45) NOT NULL,"
+    "`image_url` VARCHAR(45) NOT NULL,"
     "PRIMARY KEY (`id`),"
     "UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,"
-    "INDEX `nutriscore` (`nutriscore` ASC) VISIBLE,"
+    "INDEX `nutrition_grade_fr` (`nutrition_grade_fr` ASC) VISIBLE,"
     "CONSTRAINT `key_start` FOREIGN KEY (`id`)"
         "REFERENCES `purbeurre`.`storage` (`start_food`),"
     "CONSTRAINT `key_substitute`FOREIGN KEY (`id`)"
@@ -39,15 +42,14 @@ TABLES['food_datas'] = (
     ") ENGINE = InnoDB;")
 
 TABLES['categories'] = (
-    "CREATE TABLE IF NOT EXISTS `categories` ("
-    "`category_id` INT NOT NULL,"
-    "`category` VARCHAR(45) NOT NULL,"
-    "PRIMARY KEY (`category_id`, `category`),"
-    "UNIQUE INDEX `category` (`category` ASC) VISIBLE,"
-    "CONSTRAINT `Key_food`"
-        "FOREIGN KEY (`category_id`)"
+  "CREATE TABLE IF NOT EXISTS `categories` ("
+  "`category_id` INT NOT NULL AUTO_INCREMENT,"
+  "`category` VARCHAR(100) NOT NULL,"
+  "PRIMARY KEY (`category_id`, `category`),"
+  "UNIQUE INDEX `category` (`category` ASC) VISIBLE,"
+  "CONSTRAINT `Key_food` FOREIGN KEY (`category_id`)"
         "REFERENCES `purbeurre`.`food_datas` (`id`)"
-    ") ENGINE = InnoDB;")
+  ") ENGINE = InnoDB;")
 
 cnx = mysql.connector.connect(user='root', password='Arnaud31',
                                  host='127.0.0.1',
@@ -57,7 +59,7 @@ cursor = cnx.cursor()
 def create_database(cursor):
     try:
         cursor.execute(
-            "CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(purbeurre))
+            "CREATE DATABASE {} DEFAULT CHARACTER SET `utf8`".format(purbeurre))
     except mysql.connector.Error as err:
         print("Failed creating database: {}".format(err))
         exit(1)
@@ -86,6 +88,20 @@ for table_name in TABLES:
             print(err.msg)
     else:
         print("OK")
+
+def validate_string(val):
+   if val != None:
+        if type(val) is int:
+            #for x in val:
+            #   print(x)
+            return str(val).encode('utf-8')
+        else:
+            return val
+
+
+for i, item in enumerate(json_obj):
+    categories_fr = validate_string(item.get("categories", None))
+    cursor.execute(("INSERT INTO `categories` (category) VALUES (%(categories_fr)s)"), (categories_fr))
 
 cursor.close()
 cnx.close()
